46169e07ad08268a9af2ce8891604b920a859afa
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-01 07:09:52 -0700

    test 040

--- src/run.py
+++ src/run.py
@@ -1,6 +1,10 @@
 import os
 
 
+def engine():
+    pass
+
+
 def run():
     REPO_ROOT_DIR = os.environ['GITHUB_WORKSPACE']
     WORKSPACE_DIR = os.path.abspath(os.path.join(REPO_ROOT_DIR, '..', 'lineosaurus-workspace'))
@@ -10,3 +14,7 @@ def run():
     print(repr(REPO_ROOT_DIR))
     print(repr(WORKSPACE_DIR))
     print(repr(SHOW_CREDIT))
+
+    print(repr(os.listdir(WORKSPACE_DIR)))
+    for i, f in enumerate(os.listdir(WORKSPACE_DIR), 1):
+        print(i, f)
