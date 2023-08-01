4883065e9115c19644410386deef41a4edc364cf
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-31 19:05:23 -0700

    finished get_clone_urls

--- src/get_clone_urls.py
+++ src/get_clone_urls.py
@@ -1,5 +1,9 @@
+import json
 
 
-
-if __name__ == '__main__':
-    main()
+def get_clone_urls(raw):
+    parsed = json.loads(raw)
+    urls = []
+    for i in parsed:
+        urls.append(i['url'] + '.git')
+    return urls
