b60ac1b4dd95edaa5a216c3ea1d84af1759e273b
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-01 11:26:38 -0700

    test046

--- src/run.py
+++ src/run.py
@@ -1,5 +1,9 @@
 import os
 import re
+import subprocess as sp
+from datetime import datetime
+
+from mykit.kit.text import byteFmt
 
 from src.get_options import get_options
 
@@ -12,12 +16,74 @@ def get_readme(REPO_ROOT_DIR):
     raise FileNotFoundError('README.md not found.')
 
 
-def engine():
-    import time
-    return time.time()
+def engine(WORKSPACE_DIR, OPTIONS):
+
+    total_line, total_size = 0, 0
+
+    def rec(path):
+        line, size = 0, 0
+        for i in os.listdir(path):
+            p = os.path.join(path, i)
+            if os.path.isfile(p):
+                with open(p, 'r', encoding='utf-8') as f: line += len(f.read().split('\n'))
+                size += os.path.getsize(p)
+            else:
+                _line, _size = rec(p)
+                line += _line
+                size += _size
+        return line, size
+    
+    for repo in os.listdir(WORKSPACE_DIR):
+        repo_path = os.path.join(repo)
+        LINE, SIZE = rec(repo_path)
+        total_line += LINE
+        total_size += SIZE
+
+    return f'{total_line} {byteFmt(total_size)}'
+    text = f"""
+# {datetime.now().strftime('%b %d, %Y')}, 300,000 lines across foo bar repositories
+
+## Lines of code
+
+```shell
+abcd-12312  ▆▆▆▆▆▆▆▆▆▆▆               50%  132,412 lines
+ad-312      ▆▆▆▆▆▆                        132,412 lines
+ab          ▆                                 132,412 lines
+abcd-12312  ▆                                 132,412 lines
+abcd-12312  ▆                                 132,412 lines
+```
+
+### Top languages
+
+```css
+.py      ▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆▆  90%  13k lines
+.ts      ▆▆▆▆▆▆                        43%  43k lines
+.tx      ▆                                 43%  22k lines
+.foobar  ▆                                 43%  1k lines
+.scsc    ▆                                 43%  433 lines
+```
+
+### Statistics
+
+```markdown
+number of repo     132
+lines of code      213,231
+number of files    231
+number of stars    32
+number of commits  141,241
+total size  141,241
+top language  141,241
+```
+
+Counted by [Line-O-Saurus](https://github.com/nvfp/Line-O-Saurus)🦕
+"""
+    return text
 
 
 def run():
+
+    ## Installing dependencies
+    sp.run(['pip', 'install', 'mykit==6.0.0'])
 
     REPO_ROOT_DIR = os.environ['GITHUB_WORKSPACE']
     WORKSPACE_DIR = os.path.abspath(os.path.join(REPO_ROOT_DIR, '..', 'lineosaurus-workspace'))
@@ -36,7 +102,7 @@ def run():
     )
 
     README = get_readme(REPO_ROOT_DIR)
-    TEXT = engine()
+    TEXT = engine(WORKSPACE_DIR, OPTIONS)
 
     with open(README, 'w') as f:
         f.write(TEXT)
