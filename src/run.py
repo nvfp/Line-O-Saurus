b43b80adeb2fce47886b425d29404d5397539603
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-01 07:06:04 -0700

    test run

--- src/run.py
+++ src/run.py
@@ -4,6 +4,9 @@ import os
 def run():
     REPO_ROOT_DIR = os.environ['GITHUB_WORKSPACE']
     WORKSPACE_DIR = os.path.abspath(os.path.join(REPO_ROOT_DIR, '..', 'lineosaurus-workspace'))
+    
+    SHOW_CREDIT = os.environ['SHOW_CREDIT']
 
     print(repr(REPO_ROOT_DIR))
     print(repr(WORKSPACE_DIR))
+    print(repr(SHOW_CREDIT))
