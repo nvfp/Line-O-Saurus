f1cd4c140777a936c52147be66502ac69042d5ad
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-04 08:29:59 -0700

    finished get_options

---
+++ src/get_options/get_options.py
@@ -0,0 +1,176 @@
+import os
+import re
+
+from src.constants import CARDS
+from get_options.yml_custom_parser import parse_dict, parse_list
+
+
+"""
+Notes:
+
+In both of these cases, the value field is empty:
+foo: |
+
+bar: |-
+
+
+`foo` and `bar` values will be evaluated as an empty string.
+
+
+Extra note: for custom-title, "|-" is recommended.
+"""
+
+
+## The default values are given for testing purposes
+def get_options(
+    ONLY_TYPE='',
+    IGNORE_TYPE='',
+    HEADER='',
+    FOOTER='',
+    CUSTOM_TITLE='',
+    NUM_SHOWN='5',
+    SHOW_APPROX='true',
+    CARD_TITLES='',
+    CARD_ORDER='',
+    PREFER_EXTENSION='true',
+    SHOW_CREDIT='true',
+):
+    
+    REPO_ROOT_DIR = os.environ['GITHUB_WORKSPACE']
+
+    class OPTIONS: ...
+
+    ## only-type
+    if ONLY_TYPE == '':  # If `null` in YAML
+        OPTIONS.ONLY_TYPE = None
+    else:
+        try:
+            only_type = parse_list(ONLY_TYPE)
+        except SyntaxError:
+            raise AssertionError('Invalid only-type value.')
+        if (type(only_type) is not list) or (len(only_type) == 0):
+            raise AssertionError('Invalid only-type value.')
+        for i in only_type:
+            if type(i) is not str:
+                raise AssertionError('Invalid only-type value.')
+            if not re.match(r'^\.\w+$', i):
+                raise AssertionError('Invalid only-type value.')
+        OPTIONS.ONLY_TYPE = only_type
+
+    ## ignore-type
+    if IGNORE_TYPE == '':
+        OPTIONS.IGNORE_TYPE = None
+    else:
+        try:
+            ignore_type = parse_list(IGNORE_TYPE)
+        except SyntaxError:
+            raise AssertionError('Invalid ignore-type value.')
+        if (type(ignore_type) is not list) or (len(ignore_type) == 0):
+            raise AssertionError('Invalid ignore-type value.')
+        for i in ignore_type:
+            if type(i) is not str:
+                raise AssertionError('Invalid ignore-type value.')
+            if not re.match(r'^\.\w+$', i):
+                raise AssertionError('Invalid ignore-type value.')
+        OPTIONS.IGNORE_TYPE = ignore_type
+    
+    ## header
+    if HEADER == '':
+        OPTIONS.HEADER = ''
+    else:
+        header = os.path.join(REPO_ROOT_DIR, HEADER)
+        if not os.path.isfile(header):
+            raise AssertionError('Invalid header value.')
+        with open(header, 'r') as f:
+            OPTIONS.HEADER = f.read()
+    
+    ## footer
+    if FOOTER == '':
+        OPTIONS.FOOTER = ''
+    else:
+        footer = os.path.join(REPO_ROOT_DIR, FOOTER)
+        if not os.path.isfile(footer):
+            raise AssertionError('Invalid footer value.')
+        with open(footer, 'r') as f:
+            OPTIONS.FOOTER = f.read()
+
+    ## custom-title
+    if CUSTOM_TITLE == '':
+        ## This default is mirroring the one in the README
+        OPTIONS.CUSTOM_TITLE = "(_DATE_) _LINE_ lines of code stretch through _OWNER_'s repositories."
+    else:
+        OPTIONS.CUSTOM_TITLE = CUSTOM_TITLE
+
+    ## num-shown
+    try:
+        num_shown = int(NUM_SHOWN)
+        if num_shown < 1:
+            raise AssertionError('Invalid num-shown value.')
+        OPTIONS.NUM_SHOWN = num_shown
+    except ValueError:
+        raise AssertionError('Invalid num-shown value.')
+
+    ## show-approx
+    if SHOW_APPROX == 'true':
+        OPTIONS.SHOW_APPROX = True
+    elif SHOW_APPROX == 'false':
+        OPTIONS.SHOW_APPROX = False
+    else:
+        raise AssertionError('Invalid show-approx value.')
+
+    ## card-titles
+    if CARD_TITLES == '':
+        ## This default is mirroring the one in the README
+        OPTIONS.CARD_TITLES = {
+            'line': "Lines of code",
+            'type': "Languages",
+            'star': "Stargazers",
+            'stat': "_OWNER_'s statistics",
+        }
+    else:
+        try:
+            card_titles = parse_dict(CARD_TITLES)
+        except SyntaxError:
+            raise AssertionError('Invalid card-titles value.')
+        if (type(card_titles) is not dict) or (len(card_titles) == 0):
+            raise AssertionError('Invalid card-titles value.')
+        for k, v in card_titles.items():
+            if (k not in CARDS) or (type(v) is not str):
+                raise AssertionError('Invalid card-titles value.')
+        OPTIONS.CARD_TITLES = card_titles
+
+    ## card-order
+    if CARD_ORDER == '':
+        ## This default is mirroring the one in the README
+        OPTIONS.CARD_ORDER = ['line', 'type', 'star', 'stat']
+    else:
+        try:
+            card_order = parse_list(CARD_ORDER)
+        except SyntaxError:
+            raise AssertionError('Invalid card-order value.')
+        if (type(card_order) is not list) or (len(card_order) == 0):
+            raise AssertionError('Invalid card-order value.')
+        for i in card_order:
+            if i not in CARDS:
+                raise AssertionError('Invalid card-order value.')
+        if len(card_order) != len(set(card_order)):
+            raise AssertionError('Invalid card-order value.')  # Has duplicates
+        OPTIONS.CARD_ORDER = card_order
+
+    ## prefer-extension
+    if PREFER_EXTENSION == 'true':
+        OPTIONS.PREFER_EXTENSION = True
+    elif PREFER_EXTENSION == 'false':
+        OPTIONS.PREFER_EXTENSION = False
+    else:
+        raise AssertionError('Invalid prefer-extension value.')
+
+    ## show-credit
+    if SHOW_CREDIT == 'true':
+        OPTIONS.SHOW_CREDIT = True
+    elif SHOW_CREDIT == 'false':
+        OPTIONS.SHOW_CREDIT = False
+    else:
+        raise AssertionError('Invalid show-credit value.')
+
+    return OPTIONS
---
+++ src/get_options/get_options_test.py
@@ -0,0 +1,244 @@
+import unittest
+
+import os
+
+from get_options.get_options import get_options
+
+
+class TestGetOptions(unittest.TestCase):
+
+    def test_only_type(self):
+        
+        ## Passes
+
+        OPT = get_options(ONLY_TYPE='[".txt"]')
+        self.assertEqual(OPT.ONLY_TYPE, ['.txt'])
+
+        OPT = get_options(ONLY_TYPE='[".txt", ".json"]')
+        self.assertEqual(OPT.ONLY_TYPE, ['.txt', '.json'])
+
+        OPT = get_options(ONLY_TYPE='[".txt",".json",".py"]')
+        self.assertEqual(OPT.ONLY_TYPE, ['.txt', '.json', '.py'])
+
+        OPT = get_options(ONLY_TYPE='- .foo\n')
+        self.assertEqual(OPT.ONLY_TYPE, ['.foo'])
+
+        OPT = get_options(ONLY_TYPE='- .foo\n- .bar\n')
+        self.assertEqual(OPT.ONLY_TYPE, ['.foo', '.bar'])
+
+        OPT = get_options(ONLY_TYPE='-.foo\n-.bar\n')
+        self.assertEqual(OPT.ONLY_TYPE, ['.foo', '.bar'])
+
+        OPT = get_options(ONLY_TYPE='.foo\n')
+        self.assertEqual(OPT.ONLY_TYPE, ['.foo', '.bar'])
+
+        OPT = get_options(ONLY_TYPE='.foo\n.bar\n')
+        self.assertEqual(OPT.ONLY_TYPE, ['.foo', '.bar'])
+        
+        ## Fails
+
+        with self.assertRaises(AssertionError) as ctx: get_options(ONLY_TYPE='true')
+        self.assertEqual(str(ctx.exception), 'Invalid only-type value.')
+
+        with self.assertRaises(AssertionError) as ctx: get_options(ONLY_TYPE="['x', 'y']")  # JSON uses double quotes
+        self.assertEqual(str(ctx.exception), 'Invalid only-type value.')
+
+        with self.assertRaises(AssertionError) as ctx: get_options(ONLY_TYPE='{"a": 1}')
+        self.assertEqual(str(ctx.exception), 'Invalid only-type value.')
+
+        with self.assertRaises(AssertionError) as ctx: get_options(ONLY_TYPE='[]')
+        self.assertEqual(str(ctx.exception), 'Invalid only-type value.')
+
+        with self.assertRaises(AssertionError) as ctx: get_options(ONLY_TYPE='[1, 2, 3]')
+        self.assertEqual(str(ctx.exception), 'Invalid only-type value.')
+
+        with self.assertRaises(AssertionError) as ctx: get_options(ONLY_TYPE='["a", "b"]')
+        self.assertEqual(str(ctx.exception), 'Invalid only-type value.')
+    
+    def test_ignore_type(self):
+        ## The same as only-type
+        pass
+    
+    def test_header(self):
+        
+        ## Passes
+
+        OPT = get_options(HEADER='')
+        self.assertEqual(OPT.HEADER, '')
+
+        OPT = get_options(HEADER='README.md')
+        self.assertEqual(OPT.HEADER, os.path.join(os.environ['GITHUB_WORKSPACE'], 'README.md'))
+        
+        ## Fails
+
+        with self.assertRaises(AssertionError) as ctx: get_options(HEADER='non-existing-file-124124141513414')
+        self.assertEqual(str(ctx.exception), 'Invalid header value.')
+    
+    def test_footer(self):
+        ## The same as `header`
+        pass
+    
+    def test_custom_title(self):
+        pass
+
+    def test_num_shown(self):
+        
+        ## Passes
+        
+        OPT = get_options(NUM_SHOWN='1')
+        self.assertEqual(OPT.NUM_SHOWN, 1)
+
+        OPT = get_options(NUM_SHOWN='10')
+        self.assertEqual(OPT.NUM_SHOWN, 10)
+        
+        OPT = get_options(NUM_SHOWN='1000')
+        self.assertEqual(OPT.NUM_SHOWN, 1000)
+        
+        ## Fails
+
+        with self.assertRaises(AssertionError) as ctx: get_options(NUM_SHOWN='0')
+        self.assertEqual(str(ctx.exception), 'Invalid num-shown value.')
+
+        with self.assertRaises(AssertionError) as ctx: get_options(NUM_SHOWN='true')
+        self.assertEqual(str(ctx.exception), 'Invalid num-shown value.')
+    
+    def test_show_approx(self):
+
+        ## Passes
+
+        OPT = get_options(SHOW_APPROX='true')
+        self.assertEqual(OPT.SHOW_APPROX, True)
+        
+        OPT = get_options(SHOW_APPROX='false')
+        self.assertEqual(OPT.SHOW_APPROX, False)
+        
+        ## Fails
+
+        with self.assertRaises(AssertionError) as ctx: get_options(SHOW_APPROX='')
+        self.assertEqual(str(ctx.exception), 'Invalid show-approx value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(SHOW_APPROX='123')
+        self.assertEqual(str(ctx.exception), 'Invalid show-approx value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(SHOW_APPROX='[1, 2, 3]')
+        self.assertEqual(str(ctx.exception), 'Invalid show-approx value.')
+    
+    def test_card_titles(self):
+
+        ## Passes
+
+        OPT = get_options(CARD_TITLES='{"line": "foo"}')
+        self.assertEqual(OPT.CARD_TITLES, {'line': 'foo'})
+
+        OPT = get_options(CARD_TITLES='{"line": "foo", "stat": "bar"}')
+        self.assertEqual(OPT.CARD_TITLES, {'line': 'foo', 'stat': 'bar'})
+
+        OPT = get_options(CARD_TITLES='{"size": "foo", "type": "bar"}')
+        self.assertEqual(OPT.CARD_TITLES, {'size': 'foo', 'type': 'bar'})
+        
+        OPT = get_options(CARD_TITLES='{"line": "foo", "type": "bar", "stat": "# baz", "size": "**boo**"}')
+        self.assertEqual(OPT.CARD_TITLES, {'line': 'foo', 'type': 'bar', 'stat': '# baz', 'size': '**boo**'})
+        
+        OPT = get_options(CARD_TITLES='{"size": "foo", "char": "bar", "star": "2", "cmit": "3"}')
+        self.assertEqual(OPT.CARD_TITLES, {'size': 'foo', 'char': 'bar', 'star': '2', 'cmit': '3'})
+        
+        OPT = get_options(CARD_TITLES='- line: foo\n')
+        self.assertEqual(OPT.CARD_TITLES, {'line': 'foo'})
+
+        OPT = get_options(CARD_TITLES='- line: foo\n- stat: bar\n')
+        self.assertEqual(OPT.CARD_TITLES, {'line': 'foo', 'stat': 'bar'})
+
+        OPT = get_options(CARD_TITLES='- line: foo\n-  stat:  bar\n')
+        self.assertEqual(OPT.CARD_TITLES, {'line': 'foo', 'stat': 'bar'})
+
+        OPT = get_options(CARD_TITLES='-line:foo\n')
+        self.assertEqual(OPT.CARD_TITLES, {'line': 'foo'})
+
+        OPT = get_options(CARD_TITLES='line:foo\n')
+        self.assertEqual(OPT.CARD_TITLES, {'line': 'foo'})
+
+        ## Fails
+
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_TITLES='true')
+        self.assertEqual(str(ctx.exception), 'Invalid card-titles value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_TITLES='[1, 2]')
+        self.assertEqual(str(ctx.exception), 'Invalid card-titles value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_TITLES='{}')
+        self.assertEqual(str(ctx.exception), 'Invalid card-titles value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_TITLES='{"foo": "bar"}')
+        self.assertEqual(str(ctx.exception), 'Invalid card-titles value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_TITLES='{"line": "foo", "bar": "baz"}')
+        self.assertEqual(str(ctx.exception), 'Invalid card-titles value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_TITLES='{"line": 1}')
+        self.assertEqual(str(ctx.exception), 'Invalid card-titles value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_TITLES='foo\n')
+        self.assertEqual(str(ctx.exception), 'Invalid card-titles value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_TITLES='- foo: bar\n- baz\n')
+        self.assertEqual(str(ctx.exception), 'Invalid card-titles value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_TITLES='- foo: bar\n')
+        self.assertEqual(str(ctx.exception), 'Invalid card-titles value.')
+
+    def test_card_order(self):
+
+        ## Passes
+
+        OPT = get_options(CARD_ORDER='["line"]')
+        self.assertEqual(OPT.CARD_ORDER, ['line'])
+        
+        OPT = get_options(CARD_ORDER='["line", "stat"]')
+        self.assertEqual(OPT.CARD_ORDER, ['line', 'stat'])
+        
+        OPT = get_options(CARD_ORDER='["line", "type", "size", "stat", "size", "char", "star", "cmit"]')
+        self.assertEqual(OPT.CARD_ORDER, ['line', 'type', 'size', 'stat', 'size', 'char', 'star', 'cmit'])
+        
+        OPT = get_options(CARD_ORDER='line\n')
+        self.assertEqual(OPT.CARD_ORDER, ['line'])
+        
+        OPT = get_options(CARD_ORDER='line\ntype\n')
+        self.assertEqual(OPT.CARD_ORDER, ['line', 'type'])
+
+        ## Fails
+
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_ORDER='true')
+        self.assertEqual(str(ctx.exception), 'Invalid card-order value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_ORDER='{"a": 1}')
+        self.assertEqual(str(ctx.exception), 'Invalid card-order value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_ORDER='[]')
+        self.assertEqual(str(ctx.exception), 'Invalid card-order value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_ORDER='["foo"]')
+        self.assertEqual(str(ctx.exception), 'Invalid card-order value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_ORDER='["line", "foo"]')
+        self.assertEqual(str(ctx.exception), 'Invalid card-order value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_ORDER='["line", "type", "line"]')
+        self.assertEqual(str(ctx.exception), 'Invalid card-order value.')
+        
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_ORDER='- foo\n')
+        self.assertEqual(str(ctx.exception), 'Invalid card-order value.')
+
+        with self.assertRaises(AssertionError) as ctx: get_options(CARD_ORDER='- line\n- line\n')
+        self.assertEqual(str(ctx.exception), 'Invalid card-order value.')
+
+    def test_prefer_extension(self):
+        ## The same as show-approx
+        pass
+
+    def test_show_credit(self):
+        ## The same as show-approx
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
