3dd071b0875c0e7ac9a0011af10d3a99c16aed8d
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-08 03:55:53 -0700

    finished assembler

--- src/engine/assembler.py
+++ src/engine/assembler.py
@@ -1,105 +1,32 @@
-from mykit.kit.text import num_round, num_approx
-
 from src.constants import __version__
-from src.engine.get_line_card import get_line_card
-from src.engine.get_type_card import get_type_card
-from src.engine.get_size_card import get_size_card
-from src.engine.get_stat_card import get_stat_card
-from src.engine.get_char_card import get_char_card
-from src.engine.get_star_card import get_star_card
-from src.engine.get_cmit_card import get_cmit_card
-from src.engine.get_file_card import get_file_card
-from src.engine.variables_manager import Vars, replace_vars
-from src.engine.counter import counter
 
 
-def card_factory(WORKSPACE_DIR, OPTIONS):
-
-    cards = []
-
-    for card in OPTIONS.CARD_ORDER:
-
-        if card == 'line': cards.append(get_line_card(
-            WORKSPACE_DIR, OPTIONS.ONLY_TYPE, OPTIONS.IGNORE_TYPE,
-            OPTIONS.NUM_SHOWN, OPTIONS.SHOW_APPROX, OPTIONS.CARD_TITLES['line']
-        ))
-
-        if card == 'type': cards.append(get_type_card(
-            WORKSPACE_DIR, OPTIONS.ONLY_TYPE, OPTIONS.IGNORE_TYPE,
-            OPTIONS.NUM_SHOWN, OPTIONS.SHOW_APPROX, OPTIONS.PREFER_EXTENSION, OPTIONS.CARD_TITLES['type']
-        ))
-
-        if card == 'size': cards.append(get_size_card(
-            WORKSPACE_DIR, OPTIONS.ONLY_TYPE, OPTIONS.IGNORE_TYPE,
-            OPTIONS.NUM_SHOWN, OPTIONS.CARD_TITLES['size']
-        ))
-
-        if card == 'stat': cards.append(get_stat_card(
-            WORKSPACE_DIR, OPTIONS.ONLY_TYPE, OPTIONS.IGNORE_TYPE,
-            OPTIONS.CARD_TITLES['stat']
-        ))
-
-        if card == 'char': cards.append(get_char_card(
-            WORKSPACE_DIR, OPTIONS.ONLY_TYPE, OPTIONS.IGNORE_TYPE,
-            OPTIONS.NUM_SHOWN, OPTIONS.SHOW_APPROX, OPTIONS.PREFER_EXTENSION, OPTIONS.CARD_TITLES['char']
-        ))
-
-        if card == 'star': cards.append(get_star_card(
-            OPTIONS.NUM_SHOWN, OPTIONS.SHOW_APPROX, OPTIONS.CARD_TITLES['star']
-        ))
-
-        if card == 'cmit': cards.append(get_cmit_card(
-            WORKSPACE_DIR, OPTIONS.NUM_SHOWN, OPTIONS.SHOW_APPROX, OPTIONS.CARD_TITLES['cmit']
-        ))
-
-        if card == 'file': cards.append(get_file_card(
-            WORKSPACE_DIR, OPTIONS.ONLY_TYPE, OPTIONS.IGNORE_TYPE,
-            OPTIONS.NUM_SHOWN, OPTIONS.SHOW_APPROX, OPTIONS.CARD_TITLES['file']
-        ))
-
-    return '\n\n'.join(cards)
+CREDIT = f'Counted by [Lineosaurus v{__version__}](https://github.com/nvfp/Line-O-Saurus)'
 
 
-def assembler(header, custom_title, cards, footer, show_credit):
+def assembler(header, custom_title, cards, footer, show_credit, auto_line_break):
 
-    if header == '':
-        text = header
-    else:
-        text = header + '\n\n'
+    ## header
+    text = header
 
-    ## Apparently, at the moment, the custom title cannot be an empty string.
-    text += custom_title + '\n\n'
+    ## custom-title
+    if custom_title != '':
+        if auto_line_break: text += '\n\n'
+        text += custom_title
 
-    text += cards
+    ## card-order
+    if cards != '':
+        if auto_line_break: text += '\n\n'
+        text += cards
 
+    ## footer
     if footer != '':
-        text += '\n\n' + footer
+        if auto_line_break: text += '\n\n'
+        text += footer
 
+    ## show-credit
     if show_credit:
-        text += f'\n\nCounted by [Lineosaurus](https://github.com/nvfp/Line-O-Saurus) ({__version__})'
+        if auto_line_break: text += '\n\n'
+        text += CREDIT
 
     return text
-
-
-def engine(WORKSPACE_DIR, OPTIONS):
-
-    line_per_ext, size_per_ext, char_per_ext, file_per_ext = counter(None, None, WORKSPACE_DIR)
-    TOTAL_LINE = sum(line_per_ext.values())
-    Vars._LINES_ = TOTAL_LINE
-    Vars._LINES_ROUND_ = num_round(TOTAL_LINE)
-    Vars._LINES_APPROX_ = num_approx(TOTAL_LINE)
-
-    line_per_ext, size_per_ext, char_per_ext, file_per_ext = counter(OPTIONS.ONLY_TYPE, OPTIONS.IGNORE_TYPE, WORKSPACE_DIR)
-    TOTAL_LINE = sum(line_per_ext.values())
-    Vars._LINE_ = TOTAL_LINE
-    Vars._LINE_ROUND_ = num_round(TOTAL_LINE)
-    Vars._LINE_APPROX_ = num_approx(TOTAL_LINE)
-
-    header = replace_vars(OPTIONS.HEADER)
-    custom_title = replace_vars(OPTIONS.CUSTOM_TITLE)
-    OPTIONS.CARD_TITLES = {k: replace_vars(v) for k, v in OPTIONS.CARD_TITLES.items()}
-    footer = replace_vars(OPTIONS.FOOTER)
-
-    cards = card_factory(WORKSPACE_DIR, OPTIONS)
-
-    return assembler(header, custom_title, cards, footer, OPTIONS.SHOW_CREDIT)
--- src/engine/test_assembler.py
+++ src/engine/test_assembler.py
@@ -1,19 +1,89 @@
 import unittest
 
-from src.engine.assembler import engine
+from src.engine.assembler import CREDIT, assembler
 
 
-DIR = 0
+HEADER = 'header'
+CUSTOM_TITLE = 'custom_title'
+CARDS = 'cards'
+FOOTER = 'footer'
+SHOW_CREDIT = True
+AUTO_LINE_BREAK = True
 
 
-class Test__engine(unittest.TestCase):
+class Test__assembler(unittest.TestCase):
 
-    def test_1(self):
-
-        result = ass()
+    def test_normal(self):
+        result = assembler(HEADER, CUSTOM_TITLE, CARDS, FOOTER, SHOW_CREDIT, AUTO_LINE_BREAK)
         expected = (
-            f''
+            f'{HEADER}\n\n'
+            f'{CUSTOM_TITLE}\n\n'
+            f'{CARDS}\n\n'
+            f'{FOOTER}\n\n'
+            f'{CREDIT}'
         )
+        self.assertEqual(result, expected)
+
+    def test_no_header(self):
+        result = assembler('', CUSTOM_TITLE, CARDS, FOOTER, SHOW_CREDIT, AUTO_LINE_BREAK)
+        expected = (
+            f'{CUSTOM_TITLE}\n\n'
+            f'{CARDS}\n\n'
+            f'{FOOTER}\n\n'
+            f'{CREDIT}'
+        )
+        self.assertEqual(result, expected)
+    
+    def test_no_custom_title(self):
+        result = assembler(HEADER, '', CARDS, FOOTER, SHOW_CREDIT, AUTO_LINE_BREAK)
+        expected = (
+            f'{HEADER}\n\n'
+            f'{CARDS}\n\n'
+            f'{FOOTER}\n\n'
+            f'{CREDIT}'
+        )
+        self.assertEqual(result, expected)
+    
+    def test_no_cards(self):
+        result = assembler(HEADER, CUSTOM_TITLE, '', FOOTER, SHOW_CREDIT, AUTO_LINE_BREAK)
+        expected = (
+            f'{HEADER}\n\n'
+            f'{CUSTOM_TITLE}\n\n'
+            f'{FOOTER}\n\n'
+            f'{CREDIT}'
+        )
+        self.assertEqual(result, expected)
+    
+    def test_no_footer(self):
+        result = assembler(HEADER, CUSTOM_TITLE, CARDS, '', SHOW_CREDIT, AUTO_LINE_BREAK)
+        expected = (
+            f'{HEADER}\n\n'
+            f'{CUSTOM_TITLE}\n\n'
+            f'{CARDS}\n\n'
+            f'{CREDIT}'
+        )
+        self.assertEqual(result, expected)
+
+    def test_no_credit(self):
+        SHOW_CREDIT = False
+        result = assembler(HEADER, CUSTOM_TITLE, CARDS, FOOTER, SHOW_CREDIT, AUTO_LINE_BREAK)
+        expected = (
+            f'{HEADER}\n\n'
+            f'{CUSTOM_TITLE}\n\n'
+            f'{CARDS}\n\n'
+            f'{FOOTER}'
+        )
+        self.assertEqual(result, expected)
+    
+    def test_no_auto_line_break(self):
+        AUTO_LINE_BREAK = False
+        result = assembler(HEADER, CUSTOM_TITLE, CARDS, FOOTER, SHOW_CREDIT, AUTO_LINE_BREAK)
+        expected = HEADER + CUSTOM_TITLE + CARDS + FOOTER + CREDIT
+        self.assertEqual(result, expected)
+    
+    def test_just_header(self):
+        result = assembler(HEADER, '', '', '', False, AUTO_LINE_BREAK)
+        expected = HEADER
         self.assertEqual(result, expected)
 
 
