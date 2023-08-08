5981b026a9f337de9a989283393f4b4a0e7ee5f8
Nicholas Valentinus <nvfastplease@gmail.com>
2023-08-08 10:26:16 -0700

    fix unit tests

--- src/engine/get_star_card/test.py
+++ src/engine/get_star_card/test.py
@@ -28,7 +28,7 @@ class Test__writer(unittest.TestCase):
         expected = (
             f'```{LANG}\n'
             f'foo-bar-baz  1,500 stargazers  73%  {get_bars(1500, TOTAL)}\n'
-            f'foo            500 stargazers  25%  {get_bars(500, TOTAL)}\n'
+            f'foo            500 stargazers  24%  {get_bars(500, TOTAL)}\n'
             f'foo-bar         50 stargazers   2%  {get_bars(50, TOTAL)}\n'
             '```'
         )
@@ -38,7 +38,7 @@ class Test__writer(unittest.TestCase):
         expected = (
             f'```{LANG}\n'
             f'foo-bar-baz  1,500 stargazers  73%  {get_bars(1500, TOTAL)}\n'
-            f'foo            500 stargazers  25%  {get_bars(500, TOTAL)}\n'
+            f'foo            500 stargazers  24%  {get_bars(500, TOTAL)}\n'
             f'foo-bar         50 stargazers   2%  {get_bars(50, TOTAL)}\n'
             f'bar              5 stargazers   0%  {get_bars(5, TOTAL)}\n'
             '```'
